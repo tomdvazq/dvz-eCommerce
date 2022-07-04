833b6851b8b3dd3d363bfd8512766085a07502ca
timi <tomdvazq@gmail.com>
2022-07-04 18:03:04 -0300

    add changes in filters

--- js/code.js
+++ js/code.js
@@ -81,3 +81,20 @@ accordionItemHeader.forEach(accordionItemHeader=>{
 })
 
 // Internal Dropdown Button //
+
+const optionMenu = document.querySelector(".select-menu");
+selectBtn = optionMenu.querySelector(".select-btn");
+options = optionMenu.querySelectorAll(".option"),
+sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle('active'));
+
+options.forEach(option =>{
+    option.addEventListener('click', ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+        console.log(selectedOption);
+
+        optionMenu.classList.remove('active')
+    })
+})
